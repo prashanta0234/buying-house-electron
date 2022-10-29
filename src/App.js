b6af954979c0347bc7ef751a113cf2e78892bc20
Nishikaru <mdsajed001@gamil.com>
2022-10-29 06:07:55 +0600

    rout setup for addProudcts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./component/Login/Login";
 
 import { Routes, Route } from "react-router-dom";
 import BuyerList from "./component/BuyerList";
+import AddProduct from "./component/AddProduct/AddProduct";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/addItem" element={<AddItem />} />
         <Route path="/buyerlist" element={<BuyerList />} />
+        <Route path="/add-products" element={<AddProduct/>} />
       </Routes>
     </>
   );
